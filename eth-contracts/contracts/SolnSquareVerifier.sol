2007cb2e3d0ffcdd1dabd1a4409a3c257e3b8419
Jio C. Zitlalpopocatl <jio@cosource.com.au>
2022-08-23 22:00:47 -0500

    Implemented SolnSquareVerifier contract that inherits from ERC721Mintable class

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,85 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+import "./verifier.sol";
+import "./ERC721Mintable.sol";
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CustomERC721Token
+{
+    using SafeMath for uint256;
+    Verifier verifierContract = new Verifier();
 
+    // Define a variable to be used as a Solution index
+    uint256 solns;
 
+    constructor() public
+    {
+        solns = 0;
+    }
 
-// TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        Verifier.proof proof; // We need a property of the type Verifier.Proof to pass later to the method verifyTx
+        uint[2] input; // We will also need to pass the input parameter to the verifyTx method
+    }
 
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solutions {
+        Solution solution; 
+        uint index;
+        address senderAddress;
+    }
 
-// TODO define an array of the above struct
+    // TODO define an array of the above struct
+    Solutions[] solutionsArray;
 
+    // TODO define a mapping to store unique solutions submitted
+    mapping(uint => bool) private uniqueSolutions;
 
-// TODO define a mapping to store unique solutions submitted
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded();
 
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(Verifier.Proof memory proof, uint[] memory input) public 
+    {
+        uint256 a = 1;
+        solns.add(a);
 
+        
 
-// TODO Create an event to emit when a solution is added
+        Solutions memory soln = new Solution {
+            proof: proof,
+            index: solns,
+            senderAddress: msg.sender
+        };
 
+        solutionsArray.push(soln);
 
+        // Emit the appropiate event
+        emit SolutionAdded();
+    }
+    
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mintNft(address to, uint256 tokenId) public 
+    {
+        bool isUnique = false;
 
-// TODO Create a function to add the solutions to the array and emit the event
+        for (uint256 index = 0; index < solutionsArray.length; index++) {
+            if (solutionsArray[index].senderAddress == to) {
 
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                // Make sure the solution is unique
+                if (!uniqueSolutions[solutionsArray[index].index]) {
+                    // Verify the solution:
+                    bool verified = verifierContract.verifyTx(solutionsArray[index].solution.proof, solutionsArray[index].solution.input);
+                    if (verified) {
+                        uniqueSolutions[solutionsArray[index].index] = true;
+                        isUnique = true;
+                        break;   
+                    }
+                }
+            }
+        }
+    }
+}
